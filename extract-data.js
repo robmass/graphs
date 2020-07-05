5e511470109d056e158ba243e6b2d674d6bdf567
robmass <robertomassimini@hotmail.com>
2020-07-05 11:46:36 +0200

    Added check on startdate and enddate and management of start date / end date not in data array

--- extract-data.js
+++ extract-data.js
@@ -1,21 +1,36 @@
 module.exports = function extractData(start_date, end_date, data) {
   const startDate = Date.parse(start_date);
   const endDate = Date.parse(end_date);
-  const startIndex = binarySearch(startDate, data, 0, data.length - 1);
-  const endIndex = binarySearch(endDate, data, 0, data.length - 1);
+  if (startDate > endDate) {
+    throw "Start Date must be less or equal to End Date";
+  }
+  const compareFn = (data, index) => Date.parse(data[index].x);
+  const startIndex = binarySearch(
+    startDate,
+    data,
+    0,
+    data.length - 1,
+    compareFn
+  );
+  const endIndex = binarySearch(endDate, data, 0, data.length - 1, compareFn);
   return data.slice(startIndex, endIndex + 1);
 };
 
-const binarySearch = (key, data, leftIndex, rightIndex) => {
+const binarySearch = (key, data, leftIndex, rightIndex, compareFn) => {
   if (leftIndex > rightIndex) {
     return false;
   }
+
+  if (leftIndex == rightIndex) {
+    return leftIndex;
+  }
   const middleIndex = Math.floor((leftIndex + rightIndex) / 2);
-  if (key === Date.parse(data[middleIndex].x)) {
+
+  if (key === compareFn(data, middleIndex)) {
     return middleIndex;
-  } else if (key < Date.parse(data[middleIndex].x)) {
-    return binarySearch(key, data, leftIndex, middleIndex);
+  } else if (key < compareFn(data, middleIndex)) {
+    return binarySearch(key, data, leftIndex, middleIndex, compareFn);
   } else {
-    return binarySearch(key, data, middleIndex, rightIndex);
+    return binarySearch(key, data, middleIndex + 1, rightIndex, compareFn);
   }
 };
